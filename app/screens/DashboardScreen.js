8a2e5e3c2909cdcac464de7ab1d81ac723002c76
jason2000-cpu <jasonmwangi2000@gmail.com>
2023-03-07 06:26:39 +0300

    resolved version errors

--- app/screens/DashboardScreen.js
+++ app/screens/DashboardScreen.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+
 
 
 import Colors from '../config/Colors';
+
+const BarIcon = <FontAwesome5 name={'bars'} brand solid />;
+const SearchIcon = <FontAwesome5 name={'search'} brand />;
+const BellIcon = <FontAwesome5 name={'bell'} brand solid />;
+{/* <FontAwesomeIcon icon="fa-sharp fa-solid fa-magnifying-glass" /> */}
+
 
 function DashboardScreen(props) {
     return (
         <View style={styles.container}>
             <View style={styles.header}>
-                <Text style={{marginRight: 20, fontSize:30}}>icon</Text>
+                <Text style={{marginRight: 20, fontSize:30}}>{BarIcon}</Text>
                 <Text style={{marginRight: 20, fontSize:30, color:Colors.primary}}>Duka  Books</Text>
-                <Text style={{marginRight: 20, fontSize:30}}>icon</Text>
-                <Text style={{marginRight: 20, fontSize:30}}>icon</Text>
+                <Text style={{marginRight: 20, fontSize:30}}>{SearchIcon}</Text>
+                <Text style={{marginRight: 20, fontSize:30}}>{BellIcon}</Text>
             </View>
         </View>
     );
@@ -20,7 +28,7 @@ function DashboardScreen(props) {
 const styles = StyleSheet.create({
     container:{
         flex: 1,
-        backgroundColor: Colors.secondary
+        backgroundColor: Colors.secondary,
     },
     header:{
         paddingTop: 30,
