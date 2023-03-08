82cc4fec4717a59646e04e78a360488829c98f0c
jason2000-cpu <jasonmwangi2000@gmail.com>
2023-03-08 19:36:12 +0300

    renovated both login and signin UI/UX

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,79 +1,92 @@
 import React from 'react';
-import { Text, View, StyleSheet, TextInput } from 'react-native';
+import { Text, View, StyleSheet, TextInput, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
+
 
 import Colors from '../config/Colors';
+
+
+
+const userIcon = <FontAwesome5 name={'user'} brand solid color={Colors.primary} />;
+const passwordIcon = <FontAwesome5 name={'lock'} brand />;
+
 
 function LoginScreen({navigation}) {
     return (
         <View style={styles.container}>
-           <View style={styles.logo_sec}>
+            <View style={{backgroundColor:Colors.primary, height:"35%", borderBottomRightRadius: 50}}>
+               <View style={styles.regBtn}>
+                    <Text style={{textAlignVertical: 'center', fontSize: 18}}>Don't have and account?</Text>
+                    <Text style={{borderWidth:1, paddingHorizontal: 10, marginLeft:15,elevation:6, backgroundColor:Colors.grey, borderRadius: 3, fontSize:18, fontWeight:'400' }}  onPress={()=>navigation.navigate('Register') }>Get Started</Text>
+               </View>
+            <View style={styles.logo_sec}>
                 <Text style={styles.logo}>DB</Text>
                 <Text style={styles.logo_txt}>Duka  Books</Text>
                 <Text style={styles.logo_ad}>your daily transactions kept safe</Text>
-           </View>
-           <View style={styles.form_sec}>
-              <Text style={{ fontSize: 25, fontWeight:'bold', color: Colors.primary ,marginLeft:130, marginBottom: 40}}>LOGIN</Text>
-            <View style={styles.form}>
-                    <View style={{marginBottom: 9}}>
-                        <Text style={{marginBottom: 4,fontSize:20}}>username</Text>
-                        <TextInput  style={{paddingLeft: 9, borderWidth:2, borderRadius: 5, borderColor:Colors.primary}} placeholder='username'/>
-                    </View>
-                    <View style={{marginBottom: 9}}>
-                        <Text style={{marginBottom: 4, fontSize: 20}}>Password</Text>
-                        <TextInput  style={{paddingLeft: 9, borderWidth:2, borderRadius: 5, borderColor:Colors.primary}} placeholder='Repeat password'/>
-                    </View>
-                    <View style={{backgroundColor:Colors.primary, borderWidth: 2,borderRadius: 5, height: 30,marginHorizontal: 25}}>
-                        <Text  style={{paddingLeft:110, color:Colors.secondary, fontSize: 20, paddingTop: 2}} onPress={()=>navigation.navigate('Dashboard')}>LOGIN</Text>
-                    </View>
-                    <Text>Or</Text>
-                    <View style={{backgroundColor:Colors.secondary, borderWidth: 2,borderRadius: 5, height: 30,marginHorizontal: 25}}>
-                        <Text style={{paddingLeft:110, fontSize: 20, paddingTop: 2}} onPress={()=> navigation.navigate("Register")}>SIGNUP</Text>
-                    </View>
             </View>
+            </View>
+            <View style={styles.logForm}>
 
-           </View>
+                <View style={{marginBottom: 9}}>
+                    <Text style={{marginBottom: 4,fontSize:20}}>{userIcon}</Text>
+                    <TextInput  style={{paddingLeft: 9, borderBottomWidth:1, borderRadius: 5, borderColor:Colors.primary}} placeholder='username'/>
+                </View>
+                <View style={{marginBottom: 9}}>
+                    <Text style={{marginBottom: 4,fontSize:20}}>{passwordIcon}</Text>
+                    <TextInput  style={{paddingLeft: 9, borderBottomWidth:1, borderRadius: 5, borderColor:Colors.primary}}  placeholder='password'/>
+                </View>
+
+                <View style={{alignItems:'center'}}>
+                    <Text style={{marginTop: 30}}>Fogort password?</Text>
+                    <Text style={{borderWidth:1, textAlign:'center',  marginTop: 20, width: "90%", color: Colors.white, backgroundColor:Colors.primary, elevation:10, borderRadius: 5, fontSize: 24, paddingVertical:3}} onPress={()=>navigation.navigate('Dashboard')}>Login</Text>
+                </View>
+
+                <View style={{ marginTop: 50,flexDirection: 'row', alignItems: 'center'}}>
+                    <View style={{flex: 1, height: 1, backgroundColor: 'black'}} />
+                    <View>
+                        <Text style={{paddingHorizontal:8, textAlign: 'center', fontSize: 15}}>Or Signin with</Text>
+                    </View>
+                    <View style={{flex: 1, height: 1, backgroundColor: 'black'}} />
+                </View>
+                <View style={{elevation: 1, backgroundColor: Colors.secondary,borderRadius: 7,flexDirection:'row',alignContent:'center', justifyContent:'space-around', marginLeft: 25,marginTop:50, borderWidth: .5, width: '80%', paddingHorizontal: 30, paddingVertical: 3}}>
+                    <Image source={require('../assets/google-color-icon.png')} style={{width: 30, height:30}} />
+                    <Text style={{textAlignVertical:'center'}}>Sign up with Google</Text>
+                </View>
+
+
+
+
+            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container:{
-        backgroundColor: Colors.primary,
-        flex: 1,
-        justifyContent:'flex-end'
-    
+        flex:1
+    },
+    regBtn:{
+        flexDirection:'row',
+        justifyContent:'flex-end',
+        marginTop:35,
+        marginRight:10
     },
     logo_sec:{
-        position: 'absolute',
-        top: 50,
-        left: 100,
+        alignItems:'center',
+        marginTop:20
     },
     logo:{
-        fontSize: 100,
-        
+        fontSize:70
     },
-   logo_txt:{
-        position:'absolute',
-        top: 90,
-        left: 2,
-        color:Colors.white,
-        fontSize: 25
-    },
-    logo_ad:{
-        color:Colors.white,
-        fontWeight:'bold',
-        opacity:.7
-    },
-    form_sec:{
-        padding: 10,
-        backgroundColor: Colors.secondary,
-        borderWidth:2,
-        borderColor: Colors.secondary,
-        borderTopEndRadius: 40,
-        borderTopLeftRadius: 40,
-        flex: .6
+    logForm:{
+       borderWidth:1,
+        // borderTopLeftRadius:60,
+        paddingTop: 50,
+        paddingHorizontal: 15,
+        height: "69%",
+        borderColor: 'black'
     }
 
 })
--- app/screens/RegisterScreen.js
+++ app/screens/RegisterScreen.js
@@ -1,35 +1,36 @@
-import React from 'react';
-import { Text, View, StyleSheet, Button, TextInput, Image } from 'react-native';
+import { Text, StyleSheet, View, TextInput, Image} from 'react-native'
+import React, { Component } from 'react'
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
 
 
-import Colors from '../config/Colors';
-
+import Colors from '../config/Colors'
 
 
 const userIcon = <FontAwesome5 name={'user'} brand solid color={Colors.primary} />;
 const emailIcon = <FontAwesome5 name={'envelope'} brand solid  color={Colors.primary}/>;
 const phoneIcon = <FontAwesome5 name={'phone'} brand  color={Colors.primary}/>;
 const passwordIcon = <FontAwesome5 name={'lock'} brand />;
-const googleIcon = <FontAwesome5 name={'google-plus-g'} brand size={35}/>
 
 
-
-function RegisterScreen({navigation}) {
+ export default function RegisterScreen({navigation}) {
     return (
-        <View style={styles.container}>
-           <View style={styles.logo_sec}>
+      <View style={styles.container}>
+      <View style={{backgroundColor:Colors.primary, height:"31%", borderBottomRightRadius: 50}}>
+            <View style={styles.loginBtn}>
+                <Text style={{textAlignVertical:'center', fontSize:18}}>Already have an account?</Text>
+                <Text style={{borderWidth:1, paddingHorizontal: 10, marginLeft:15,elevation:6, backgroundColor:Colors.grey, borderRadius: 3, fontSize:18, fontWeight:'400' }}  onPress={()=>navigation.navigate('Login') }>LOGIN</Text>
+            </View>
+            <View style={styles.logo_sec}>
                 <Text style={styles.logo}>DB</Text>
                 <Text style={styles.logo_txt}>Duka  Books</Text>
                 <Text style={styles.logo_ad}>your daily transactions kept safe</Text>
-           </View>
-           <View style={styles.form_sec}>
-               <View style={{flexDirection:'row', justifyContent: 'space-around'}}>
-                  <Text style={{borderWidth: 2, fontSize: 20, paddingHorizontal: 50, fontWeight:'bold',backgroundColor:Colors.primary, color: Colors.white ,marginLeft:1, marginBottom: 40}}>SIGNUP</Text>
-                  <Text style={{borderWidth:2, fontSize: 20, paddingHorizontal: 50, fontWeight:'bold', backgroundColor: Colors.white, color: Colors.primary, marginBottom: 40}}>SIGNIN</Text>
-               </View>
-            <View style={styles.form}>
+            </View>
+
+      </View>
+      <View style={styles.regForm}>
+
+
                     <View style={{marginBottom: 9}}>
                         <Text style={{marginBottom: 4,fontSize:20}}>{userIcon}</Text>
                         <TextInput  style={{paddingLeft: 9, borderBottomWidth:1, borderRadius: 5, borderColor:Colors.primary}} placeholder='username'/>
@@ -40,23 +41,16 @@ function RegisterScreen({navigation}) {
                     </View>
                     <View style={{marginBottom: 9}}>
                         <Text  style={{marginBottom: 4,fontSize:20}}>{phoneIcon}</Text>
-                        <TextInput  style={{paddingLeft: 9, borderBottomWidth:1, borderRadius: 5, borderColor:Colors.primary}} placeholder='username'/>
+                        <TextInput  style={{paddingLeft: 9, borderBottomWidth:1, borderRadius: 5, borderColor:Colors.primary}} value='+254' placeholder='phone number'/>
                     </View>
                     <View style={{marginBottom: 9}}>
                         <Text style={{marginBottom: 4,fontSize:20}}>{passwordIcon}</Text>
-                        <TextInput  style={{paddingLeft: 9, borderBottomWidth:1.5, borderRadius: 5, borderColor:Colors.primary}}  placeholder='{passwordIcon}'/>
+                        <TextInput  style={{paddingLeft: 9, borderBottomWidth:1, borderRadius: 5, borderColor:Colors.primary}}  placeholder='password'/>
                     </View>
-                    {/* <View style={{marginBottom: 9}}>
-                        <Text style={{marginBottom: 4, fontSize: 20}}>Password</Text>
-                        <TextInput  style={{paddingLeft: 9, borderWidth:2, borderRadius: 5, borderColor:Colors.primary}} placeholder='Repeat password'/>
-                    </View> */}
-                    {/* <View style={{backgroundColor:Colors.primary, borderWidth: 2,borderRadius: 5, height: 30,marginHorizontal: 25}}>
-                        <Text  style={{paddingLeft:110, color:Colors.secondary, fontSize: 20, paddingTop: 2}} onPress={()=>navigation.navigate('Dashboard')}>SIGNUP</Text>
+
+                    <View style={{alignItems:'center'}}>
+                    <Text style={{borderWidth:1, textAlign:'center',  marginTop: 20, width: "90%", color: Colors.white, backgroundColor:Colors.primary, elevation:10, borderRadius: 5, fontSize: 20, paddingVertical:3}} onPress={()=>navigation.navigate('Dashboard')}>SIGNUP</Text>
                     </View>
-                    <Text>Or</Text>
-                    <View style={{backgroundColor:Colors.secondary, borderWidth: 2,borderRadius: 5, height: 30,marginHorizontal: 25}}>
-                        <Text style={{paddingLeft:110, fontSize: 20, paddingTop: 2}} onPress={()=>navigation.navigate('Login') } >SIGNIN</Text>
-                    </View> */}
 
                     <View style={{ marginTop: 50,flexDirection: 'row', alignItems: 'center'}}>
                         <View style={{flex: 1, height: 1, backgroundColor: 'black'}} />
@@ -65,58 +59,48 @@ function RegisterScreen({navigation}) {
                         </View>
                         <View style={{flex: 1, height: 1, backgroundColor: 'black'}} />
                     </View>
-
-                    {/* <Text style={{marginTop:50, borderWidth: 1, width: '80%', margin: 10, paddingHorizontal: 30, paddingVertical: 3}}><Text>{googleIcon}</Text> Signup with Google</Text> */}
-                    <View style={{ border,borderRadius: 7,flexDirection:'row',alignContent:'center', justifyContent:'space-around', marginLeft: 25,marginTop:50, borderWidth: .5, width: '80%', paddingHorizontal: 30, paddingVertical: 3}}>
-                        <Image source={require('../assets/google-color-icon.png')} style={{width: 40, height:40}} />
+                    <View style={{elevation: 1, backgroundColor: Colors.secondary,borderRadius: 7,flexDirection:'row',alignContent:'center', justifyContent:'space-around', marginLeft: 25,marginTop:50, borderWidth: .5, width: '80%', paddingHorizontal: 30, paddingVertical: 3}}>
+                        <Image source={require('../assets/google-color-icon.png')} style={{width: 30, height:30}} />
                         <Text style={{textAlignVertical:'center'}}>Sign up with Google</Text>
                     </View>
-                    <Text></Text>
-            </View>
+                    
 
-           </View>
-        </View>
-    );
+
+
+
+
+
+
+
+      </View>
+      </View>
+    );   
 
 }
+
 const styles = StyleSheet.create({
     container:{
-        backgroundColor: Colors.primary,
-        flex: 1,
-        justifyContent:'flex-end'
-    
+        flex:1,
+    },
+    loginBtn:{
+        justifyContent:'flex-end',
+        flexDirection:'row',
+        paddingTop: 40,
+        marginRight: 10
     },
     logo_sec:{
-        position: 'absolute',
-        top: 50,
-        left: 100,
+        alignItems:'center',
+        marginTop:20
     },
     logo:{
-        fontSize: 100,
-        
+        fontSize: 70
     },
-   logo_txt:{
-        position:'absolute',
-        top: 90,
-        left: 2,
-        color:Colors.white,
-        fontSize: 25
+    regForm:{
+         borderWidth:1,
+        // borderTopLeftRadius:60,
+        paddingTop: 50,
+        paddingHorizontal: 15,
+        height: "69%",
+        borderColor: 'black'
     },
-    logo_ad:{
-        color:Colors.white,
-        fontWeight:'bold',
-        opacity:.7
-    },
-    form_sec:{
-        padding: 10,
-        backgroundColor: Colors.secondary,
-        borderWidth:2,
-        borderColor: Colors.secondary,
-        borderTopEndRadius: 40,
-        borderTopLeftRadius: 40,
-        flex: .74
-    }
-
 })
-
-export default RegisterScreen;
